168bd16ee7b53189f24d3ae1c2f496e078af23ad
Hardeek6732 <47438986+Hardeek6732@users.noreply.github.com>
2019-02-07 21:50:58 -0500

    First submission

---
+++ P2T1_SalesPrediction_KimberleyHardee.py
@@ -0,0 +1,15 @@
+# Sales predictions for company
+# 2/7/2019
+# CTI-110 P2T1- Sales Prediction
+# Kimberley Hardee
+
+total_sales = float(input("Enter the projected sales"))
+
+# Calculate the profir as 23 percent of total sales.
+
+profit = total_sales * 0.23
+
+# Display the profit.
+
+print("The profit is $",format(profit,",.2f"))
+
